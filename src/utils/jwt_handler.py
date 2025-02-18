aa861198f0f707fd1e928c332f870579804191d7
Eytes <yura.shamanov2000@bk.ru>
2025-02-18 16:25:54 +0200

    splitting the code into smaller functions

--- src/utils/jwt_handler.py
+++ src/utils/jwt_handler.py
@@ -10,20 +10,17 @@ from settings import settings
 from redis_client import redis_client
 
 
-def create_jwt_token(expires_delta: timedelta) -> str:
-    """Создать JWT-токен с указанным временем жизни."""
-    payload = {
-        "iat": datetime.now(UTC),
-        "exp": datetime.now(UTC) + expires_delta,
-    }
-    return jwt.encode(
-        payload, settings.PRIVATE_KEY_PATH.read_text(), algorithm=settings.ALGORITHM
-    )
+async def is_revoked(token: str) -> bool:
+    """Проверяется был ли отозван токен. Если токена нет, токен был отозван"""
+    value = await redis_client.get_value_by_token(token)
+    return value is None
 
 
-def verify_jwt_token(token: str) -> dict:
+async def verify_jwt_token(token: str) -> dict:
     """Проверить JWT-токен и вернуть его payload."""
     try:
+        if await is_revoked(token):
+            raise ExpiredSignatureError
         return jwt.decode(
             token,
             settings.PUBLIC_KEY_PATH.read_text(),
@@ -35,22 +32,36 @@ def verify_jwt_token(token: str) -> dict:
         raise InvalidTokenHTTPException
 
 
-async def rotate_tokens_by_refresh_token(refresh_token: str) -> dict:
-    """Обновляет Refresh токен и выдает новый Access и Refresh токен."""
-    payload = verify_jwt_token(refresh_token)
-
-    # Проверяем, что переданный токен действительно является refresh-токеном
+async def is_refresh_token(token: str) -> bool:
+    """Проверяем, что токен является refresh-токеном"""
+    payload = await verify_jwt_token(token)
     token_lifetime = payload.get("exp") - payload.get("iat")
     expected_lifetime = settings.REFRESH_TOKEN_EXPIRE_DAYS * 86400
+    return token_lifetime >= expected_lifetime - 60  # Допускаем небольшую погрешность
 
-    if token_lifetime < expected_lifetime - 60:  # Допускаем небольшую погрешность
+
+def create_jwt_token(expires_delta: timedelta) -> str:
+    """Создать JWT-токен с указанным временем жизни."""
+    now =  datetime.now(UTC)
+    payload = {
+        "iat": now,
+        "exp": now + expires_delta,
+    }
+    return jwt.encode(
+        payload,
+        settings.PRIVATE_KEY_PATH.read_text(),
+        algorithm=settings.ALGORITHM,
+    )
+
+
+async def rotate_tokens_by_refresh_token(refresh_token: str) -> dict:
+    """Обновляет Refresh токен и выдает новый Access и Refresh токен."""
+
+    # Проверяем, что переданный токен действительно является refresh-токеном
+    if not await is_refresh_token(refresh_token):
         raise TokenWrongTypeHTTPException
 
-    # Если Access-токен был отозван, следовательно, и refresh-токен должен быть отозван
     access_token = await redis_client.get_value_by_token(refresh_token)
-    if access_token is None:
-        await redis_client.revoke_token(refresh_token)
-        raise InvalidTokenHTTPException
 
     await redis_client.revoke_token(access_token)
     await redis_client.revoke_token(refresh_token)
@@ -64,7 +75,7 @@ async def rotate_tokens_by_refresh_token(refresh_token: str) -> dict:
 
     await redis_client.set_token(
         new_access_token,
-        "user_id",
+        "user_id",  # TODO: сделать привязку к данным пользователя или к сессии
         settings.ACCESS_TOKEN_EXPIRE_MINUTES * 60,
     )
     await redis_client.set_token(
